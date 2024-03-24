76069d16bbbbed4f3b869e549c5721397feea0d9
Stanislav <gvaurdin777@yandex.ru>
2024-03-24 23:40:00 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_HW_5_Parallel_programming
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private bool isAnalyzing = false;
+        private CancellationTokenSource cancellationTokenSource; // класс для отмены операции анализа
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void AnalyzeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isAnalyzing)
+            {
+                isAnalyzing = true;
+                AnalyzeButton.Content = "Остановить";
+                cancellationTokenSource = new CancellationTokenSource();
+
+                string text = TextInput.Text;
+
+                // проверка состояния чек боксов и запись логики в переменную bool
+                bool includeSentences = SentencesCheckBox.IsChecked == true;
+                bool includeWords = WordsCheckBox.IsChecked == true;
+                bool includeQuestionSentences = QuestionSentencesCheckBox.IsChecked == true;
+                bool includeExclamationSentences = ExclamationSentencesCheckBox.IsChecked == true;
+
+                await AnalyzeText(text, includeSentences, includeWords, includeQuestionSentences, includeExclamationSentences, cancellationTokenSource.Token);
+
+                isAnalyzing = false;
+                AnalyzeButton.Content = "Анализировать";
+                StatusLabel.Content = "";
+            }
+            else
+            {
+                isAnalyzing = false;
+                cancellationTokenSource.Cancel();
+            }
+        }
+
+        private async Task AnalyzeText(string text, bool includeSentences, bool includeWords, bool includeQuestionSentences, bool includeExclamationSentences, CancellationToken cancellationToken)
+        {
+            try
+            {
+                StatusLabel.Content = "Выполняется анализ текста...";
+                await Task.Delay(1000, cancellationToken); // искусственная задержка для имитации длительного анализа
+
+                string report = "";
+
+                if (includeSentences)
+                {
+                    StatusLabel.Content = "Анализируем количество предложений";
+                    await Task.Delay(1000, cancellationToken);
+                    int sentenceCount = CountSentences(text);
+                    report += $"Количество предложений: {sentenceCount}\n";
+                }
+
+                if (includeWords)
+                {
+                    StatusLabel.Content = "Анализируем количество слов";
+                    await Task.Delay(1000, cancellationToken);
+                    int wordCount = CountWords(text);
+                    report += $"Количество слов: {wordCount}\n";
+                }
+
+                if (includeQuestionSentences)
+                {
+                    StatusLabel.Content = "Анализируем количество вопросительных предложений";
+                    await Task.Delay(1000, cancellationToken);
+                    int questionSentenceCount = CountQuestionSentences(text);
+                    report += $"Количество вопросительных предложений: {questionSentenceCount}\n";
+                }
+
+                if (includeExclamationSentences)
+                {
+                    StatusLabel.Content = "Анализируем количество восклицательных предложений";
+                    await Task.Delay(1000, cancellationToken);
+                    int exclamationSentenceCount = CountExclamationSentences(text);
+                    report += $"Количество восклицательных предложений: {exclamationSentenceCount}\n";
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    if (!string.IsNullOrWhiteSpace(report))
+                    {
+                        if (ShowOnScreenRadioButton.IsChecked == true)
+                        {
+                            MessageBox.Show(report, "Отчет");
+                        }
+                        else if (SaveToFileRadioButton.IsChecked == true)
+                        {
+                            SaveReportToFile(report);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выберите хотя бы один тип информации для включения в отчет", "Предупреждение");
+                    }
+                });
+            }
+            catch (OperationCanceledException)
+            {
+                // Обработка отмены операции
+                MessageBox.Show("Анализ текста отменен", "Отмена");
+            }
+        }
+
+        // методы для поиска предложений , слов, вопросительных предложений, восклицательных предложений
+        private int CountSentences(string text)
+        {
+            return Regex.Matches(text, @"[.!?]").Count;
+        }
+
+        private int CountWords(string text)
+        {
+            return Regex.Matches(text, @"\b\w+\b").Count;
+        }
+
+        private int CountQuestionSentences(string text)
+        {
+            return Regex.Matches(text, @"\b\w+[?]").Count;
+        }
+
+        private int CountExclamationSentences(string text)
+        {
+            return Regex.Matches(text, @"\b\w+[!]").Count;
+        }
+
+        private void SaveReportToFile(string report)
+        {
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, report);
+                MessageBox.Show("Отчет сохранен в файл", "Сохранение");
+            }
+        }
+    }
+}
---
+++ MainWindow_Task_2.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp_HW_5_Parallel_programming
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow_Task_2.xaml
+    /// </summary>
+    public partial class MainWindow_Task_2 : Window
+    {
+        // переменная для отслеживания наличия дубликатов
+        private bool isFind = false;
+        public MainWindow_Task_2()
+        {
+            InitializeComponent();
+        }
+
+        private void SourceDirectoryBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // создание диалогового окна для выбора директории
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                // отображение диалогового окна и получение результата выбора
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                // если пользователь выбрал директорию и нажал "OK"
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    // установка выбранного пути в текстовое поле для исходной директории
+                    SourceDirectoryTextBox.Text = dialog.SelectedPath;
+                    // установка выбранного пути в метку для отображения выбранной директории
+                    SourceDirectoryLabel.Content = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void DestinationDirectoryBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    DestinationDirectoryTextBox.Text = dialog.SelectedPath;
+                    DestinationDirectoryLabel.Content = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void CheckDuplicatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // получение путей исходной и целевой директорий из текстовых полей
+            string sourceDirectory = SourceDirectoryTextBox.Text;
+            string destinationDirectory = DestinationDirectoryTextBox.Text;
+
+            // проверка, что обе директории были выбраны
+            if (string.IsNullOrWhiteSpace(sourceDirectory) || string.IsNullOrWhiteSpace(destinationDirectory))
+            {
+                //вывод сообщения об ошибке, если одна из директорий не выбрана
+                MessageBox.Show("Выберите исходную и целевую директории");
+                return;
+            }
+
+            try
+            {
+                //вызов асинхронного метода для проверки и перемещения дубликатов
+                await CheckAndMoveDuplicatesAsync(sourceDirectory, destinationDirectory);
+                // проверка наличия дубликатов и вывод сообщения об успешном завершении
+                if (isFind)
+                    MessageBox.Show("Проверка завершена. Оригинальные файлы перемещены в целевую директорию.");
+            }
+            catch (Exception ex)
+            {
+                // вывод сообщения об ошибке, если произошла исключительная ситуация
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private async Task CheckAndMoveDuplicatesAsync(string sourceDirectory, string destinationDirectory)
+        {
+            // искусственная задержка в 2 секунды для имитации выполнения операции
+            await Task.Delay(2000);
+
+            // получение списка файлов в исходной директории
+            var files = Directory.GetFiles(sourceDirectory);
+
+            // группировка файлов по размеру
+            var groupedFiles = files.GroupBy(file => new FileInfo(file).Length);
+
+            // список для хранения имен перемещенных файлов
+            var movedFiles = new List<string>();
+
+            // перебор групп файлов
+            foreach (var group in groupedFiles)
+            {
+                // если в группе больше одного файла
+                if (group.Count() > 1)
+                {
+                    // выбор первого файла в группе как оригинального
+                    var originalFile = group.First();
+
+                    //выбор всех остальных файлов в группе как дубликатов
+                    var duplicateFiles = group.Skip(1);
+
+                    //перебор дубликатов
+                    foreach (var duplicateFile in duplicateFiles)
+                    {
+                        //формирование пути для перемещения дубликата в целевую директорию
+                        string destinationFilePath = Path.Combine(destinationDirectory, Path.GetFileName(duplicateFile));
+
+                        // проверка, что файл еще не был перемещен
+                        if (!File.Exists(destinationFilePath))
+                        {
+                            // перемещение файла в целевую директорию
+                            File.Move(duplicateFile, destinationFilePath);
+                            //добавление имени перемещенного файла в список
+                            movedFiles.Add(Path.GetFileName(destinationFilePath));
+                        }
+                    }
+                }
+            }
+
+            // если были перемещены какие-либо файлы
+            if (movedFiles.Any())
+            {
+                // формирование сообщения о перемещенных файлах
+                string movedFilesMessage = string.Join("\n", movedFiles);
+                // вывод сообщения об успешном перемещении файлов
+                MessageBox.Show($"Перемещены файлы:\n{movedFilesMessage}", "Перемещение файлов");
+                // установка флага наличия дубликатов в true
+                isFind = true;
+            }
+            else
+            {
+                // вывод сообщения о том, что дубликаты не были найдены
+                MessageBox.Show("Нет дубликатов для перемещения.", "Перемещение файлов");
+            }
+        }
+    }
+}
